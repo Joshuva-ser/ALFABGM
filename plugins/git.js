f308798ac46496310e9341dff30f53f56c2bfeef
Joshuva-ser <96343347+Joshuva-ser@users.noreply.github.com>
2022-03-25 23:55:49 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,20 +7,17 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://telegra.ph/file/f6ec3d3d845a1e3dcce39.jpg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://i.imgur.com/Halmrbv.jpeg", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Alexa bot created by Turbo*
-*Creator number : wa.me/916380260672?text=Hi%20Turbo%20Mods.%20*
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*ᴀʟᴩʜᴀ bot created by ᴊᴏꜱʜᴜᴠᴀ*
+*Creator number : wa.me/918129900380?text=Hi%20Joshuva%20Monee.%20*
 
-*Githublink :    https://github.com/TURBOHYPER/Toxic-Alexa*
+*Githublink :    https://github.com/Joshuva-ser/ALFABGM*
 
-*Audio commads :   https://github.com/TURBOHYPER/Toxic-Alexa/tree/master/uploads*
+*Audio commads :   https://github.com/Joshuva-ser/ALFABGM/tree/master/uploads*
 
 *Sticker commads : coming soon*
 
-*My channel link : https://youtube.com/c/TurboMods* 
-
-*For More Updates Subscribe The Channel ☝*
 `}) 
 
 }));
