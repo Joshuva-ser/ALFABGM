f9689cb257ac8777201ae2604bcfcea539c15eae
Joshuva-ser <96343347+Joshuva-ser@users.noreply.github.com>
2022-03-27 18:09:42 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -10,14 +10,14 @@ Asena.addCommand({pattern: 'menu', fromMe: false, desc: Lang.WP}, (async (messag
     var r_text = new Array ();
     
     
-    r_text[0] = "https://telegra.ph/file/f6ec3d3d845a1e3dcce39.jpg";
+    r_text[0] = "https://i.imgur.com/Halmrbv.jpeg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦━━━━━✨️TURBO MODS✨️━━━━━⛦
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦━━━━━ᴀʟᴩʜᴀ ꜱᴇʀ━━━━━⛦
 
 
 𝐇𝐞𝐥𝐥𝐨👋 𝐈 𝐚𝐦 𝐚 *ALPHA* 𝐛𝐨𝐭.
